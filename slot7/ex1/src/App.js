6fba3dd280b507be383400b55f7a0d077dfd94ca
dinhnhat73 <truongdinhnhat115@gmail.com>
2025-10-01 04:47:41 +0200

    add slot7

---
+++ slot7/ex1/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import Banner from './components/Banner';
+import Navbar from './components/Navar';
+function App() {
+  return (
+    <div>
+      <Banner />
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
